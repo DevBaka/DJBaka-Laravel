8fa33ef6b7257bc10b6e976bccbb1f3b3a179430
DJBaka <devbaka@protonmail.com>
2025-06-06 03:54:23 +0200

    form input

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -5,7 +5,12 @@
 
     <h2>Todo liste:</h2>
     <form wire:submit="add">
-        <input type="text" wire:model="todo">
+        <p>Jeder input wird direkt übermittelt mit .live: </p>
+        <input type="text" wire:model.live="todo"><span>Current todo: {{ $todo }} </span>
+        <p>Jeder input wird direkt nach 50ms übermittelt mit .live.debounce.50ms: </p>
+        <input type="text" wire:model.live.debounce.200ms="todo"><span>Current todo: {{ $todo }} </span>
+        <p>Der Input wird übermittelt nach dem raustabben aus der input box mit: .live.change: </p>
+        <input type="text" wire:model.live.change="todo">
         <span>Current todo: {{ $todo }} </span>
         <button type="submit">Add</button>
     </form>
