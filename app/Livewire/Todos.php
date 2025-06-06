0fb278a83ce2bad9ed443db88586bbafdc8b3b65
DJBaka <devbaka@protonmail.com>
2025-06-06 04:07:57 +0200

    Lifecycle Hooks

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -12,10 +12,29 @@ class Todos extends Component
     }
     public $todo = '';
     public $txtDebugText = 'Debug Text ändern';
-    public $todos = [
+    public $todos = [];
+
+    //Lifecycle Hooks
+
+    //Mount wird direkt ausgeführt und interuppted alles andere aus der page
+    public function mount(){
+        //Der code wird durch den Lifecycle Hook mount() direkt als erstes ausgeführt, bevor alles andere geladen wird. Das könnte genutzt werden um Daten aus der Datenbank zu laden.
+        $this->todos = [
         'Müll rausbringen',
         'Do dishes',
-    ];
+        ];
+        $this->todo = "Weiteren Punkt zur Todo hinzufügen";
+    }
+
+    // Updated wird jedesmals ausgeführt, sobald etwas geupdated wird.
+    public function updated($property, $value){
+        $this->$property = strtoupper($value);
+    }
+
+    //Hiermit wird die funktion aufgerufen, sobald die variable todo geupdated wurde.
+    public function updatedTodo(){
+        //dd('Todo liste wurde geupdatet!');
+    }
 
     public function add(){
         $this->todos[] = $this->todo;
