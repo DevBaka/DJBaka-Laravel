5b1ead54e3359e4ed2540d9a4d86885a8e5e24b8
DJBaka <devbaka@protonmail.com>
2025-06-07 20:03:16 +0200

    seperate theme in partials

---
+++ resources/views/backend/partials/footer.blade.php
@@ -0,0 +1,5 @@
+            <footer
+                class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
+                <p class="text-muted mb-1 mb-md-0">Copyright © 2025 <a href="https://www.djbaka.com"
+                        target="_blank">DJBaka</a>.</p>
+            </footer>
